c635207f24666ae25ef8ca21b5b97d6205adaaa3
Stano36 <136756503+stanislawrabel@users.noreply.github.com>
2025-08-11 10:50:22 +0200

    Add files via upload

---
+++ incurl_t.sh
@@ -0,0 +1,36 @@
+/bin/bash
+set -e
+
+# 🛠 Automatický mód
+export DEBIAN_FRONTEND=noninteractive
+export TERM=xterm
+
+set -e
+
+echo "📦 Updating Termux and installing dependencies..."
+yes "" | pkg update -y
+yes "" | pkg upgrade -y
+echo N | dpkg --configure -a
+
+pkg install -y python python2 git tsu curl
+pip install wheel
+pip install pycryptodome
+pip3 install --upgrade requests pycryptodome git+https://github.com/R0rt1z2/realme-ota
+
+echo "📥 Downloading scripts and data files..."
+REPO="https://raw.githubusercontent.com/stanislawrabel/tod/main"
+
+curl -sSL "$REPO/m.sh" -o t.sh
+curl -sSL "$REPO/models.txt" -o models.txt
+curl -sSL "$REPO/devices.txt" -o devices.txt
+
+chmod +x t.sh
+
+# 🛠️ Adding an alias for easy launch 
+if ! grep -q "alias t=" ~/.bashrc; then
+    echo "alias t='bash ~/t.sh'" >> ~/.bashrc
+    echo -e "\e[32m✅ Alias 't' has been added.\e[0m"
+fi
+source ~/.bashrc
+clear
+exit
