cc5e7db66c41aea1e5fa008fcce01ca1be47a695
Stano36 <136756503+stanislawrabel@users.noreply.github.com>
2025-08-11 10:19:43 +0200

    Add files via upload

---
+++ ota.sh
@@ -0,0 +1,13 @@
+termux-change-repo
+termux-setup-storage
+pkg  upgrade
+pkg  update
+pkg  install python2
+pkg  install python
+pkg  install git
+pkg  install tsu
+pip  install wheel
+pip  install pycryptodome
+pip3  install --upgrade requests pycryptodome git+https://github.com/R0rt1z2/realme-ota
+clear
+
